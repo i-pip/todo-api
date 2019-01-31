687995d8dc1401bee73d6a0fee394900e8ae7e5f
pip <jrphiri@gmail.com>
2019-01-31 15:21:38 +0200

    added tests for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -39,15 +39,14 @@ app.get("/todos/:id", (req, res) => {
   const { id } = req.params;
   if (!ObjectID.isValid(id)) {
     console.log("invalid object id: ", id);
-    res.status(404).send({ error: "invalid id" });
+    return res.status(404).send("invalid object id");
   }
 
   //findById
   Todo.findById(id)
     .then(todo => {
-      console.log("todo: ", todo);
       if (!todo) {
-        res.status(400).send({ error: "no todo" });
+        return res.status(404).send();
       }
 
       res.send({ todo });
@@ -74,7 +73,7 @@ app.get("/users/:id", (req, res) => {
       res.status(200).send(user);
     })
     .catch(e => {
-      res.status(400).send("Error getting user");
+      res.status(400).send("Error getting user", e);
     });
   //success
 
