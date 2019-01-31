5ae1b1074540780af8c91f44eda91dd78219e5d1
pip <jrphiri@gmail.com>
2019-01-31 14:58:52 +0200

    Added GET /todos/:id and GET /users/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
-const { User } = require("./db/user");
-const { Todo } = require("./db/todo");
+const { User } = require("./models/user");
+const { Todo } = require("./models/todo");
 
 const app = express();
 
@@ -32,6 +33,52 @@ app.get("/todos", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    console.log("invalid object id: ", id);
+    res.status(404).send({ error: "invalid id" });
+  }
+
+  //findById
+  Todo.findById(id)
+    .then(todo => {
+      console.log("todo: ", todo);
+      if (!todo) {
+        res.status(400).send({ error: "no todo" });
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send("Error getting todo");
+    });
+});
+
+app.get("/users:id", (req, res) => {
+  // req.params
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  //findById
+  User.findById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).send();
+      }
+
+      res.status(200).send(user);
+    })
+    .catch(e => {
+      res.status(400).send("Error getting user");
+    });
+  //success
+
+  //error
 });
 
 app.listen(3000, () => {
