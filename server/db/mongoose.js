3a35a7c507729dbc3bc35e325786a9f2a64c3837
pip <jrphiri@gmail.com>
2019-01-31 18:30:14 +0200

    added useNewUrlParser option for deprecation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
+  dbUrl,
+  { useNewUrlParser: true }
 );
 
 module.exports = {
